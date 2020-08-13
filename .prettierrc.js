aef41caa0679569568ec6f557a746599540f5297
NFSS10 <nuno_nfss96@hotmail.com>
2020-08-13 16:57:16 +0100

    Setup pretty

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    tabWidth: 4,
+    printWidth: 120
+};
