786d09cf6ef5c683e2da14241f9e64bebfab1b17
NFSS10 <nuno_nfss96@hotmail.com>
2020-08-13 17:58:14 +0100

    Pretty

--- tests/huffman-coding/HuffmanCoding.test.js
+++ tests/huffman-coding/HuffmanCoding.test.js
@@ -5,8 +5,22 @@ describe('Huffman Coding Tests', () => {
         const huffmanCoding = new HuffmanCoding();
 
         const encodedStr = huffmanCoding.encode('Huffman');
-        expect(huffmanCoding.charsFreq).toEqual({ H: 1, a: 1, f: 2, m: 1, n: 1, u: 1 });
-        expect(huffmanCoding.charsCoding).toEqual({ H: '000', a: '011', f: '101', m: '010', n: '100', u: '001' });
+        expect(huffmanCoding.charsFreq).toEqual({
+            H: 1,
+            a: 1,
+            f: 2,
+            m: 1,
+            n: 1,
+            u: 1
+        });
+        expect(huffmanCoding.charsCoding).toEqual({
+            H: '000',
+            a: '011',
+            f: '101',
+            m: '010',
+            n: '100',
+            u: '001'
+        });
         expect(encodedStr).toEqual('000001101101010011100');
     });
 
